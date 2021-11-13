58491c05e313e2fad766f02afc17368bd42bdbbc
Marnie K <marnie.kelly.123@gmail.com>
2021-11-13 17:29:56 -0500

    adding pseudocode and varables

---
+++ index.js
@@ -0,0 +1,58 @@
+function validatePassword (password) {
+  
+const password = ""
+
+const eightPlus = true if
+
+const lowerPresent = true if
+
+const upperPresent = true if
+
+const spCharPreseent = '!@#$%^&*()-_=+'
+
+}
+
+//   const password
+// input will be a variable named password
+// password could be a number or a string
+
+
+// const eightPlus
+// create a variable named eightPlus
+// create function that determines length named determineLength()
+// use array.length
+// if array length  is >= 8 return true
+
+// switch statement runs through remainin 4 conditions
+
+//   const lowerPreesent
+// create a variable named lowerPreesent
+// create function that determines length named determineLower()
+// Must contain at least one lower case letter
+// check  with  for  loop
+// lowerPreesent === true
+
+//const upperPresent
+// create a variable named upperPresent
+// create function that determines length named determineUppre()
+// Must contain at least one upper case letter
+// check  with  for  loop
+// upperPresent === true
+
+//const numPresent
+// create variable named numPresent
+// create function that determines length named determineNum()
+// Must contain at least one numeric value
+// check  with  for  loop
+// numberPresent === true
+
+// const spCharPreseent
+// create variable named spCharPreseent
+// create function that determines length named determineSpecial()
+// Must contain at least one special character
+// check  with  for  loop
+// spCharPreseent === true
+
+// if 
+
+module.exports = validatePassword
